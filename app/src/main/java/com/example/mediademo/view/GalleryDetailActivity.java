d162dbcdacc7600071923d6247bcecb422080c35
shiva.gunasehkaran <shivagunasehkaran@rea-group.com>
2019-09-14 01:47:02 +0800

    Added GalleryDetail activity and layout

---
+++ app/src/main/java/com/example/mediademo/view/GalleryDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.mediademo.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GalleryDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery_detail);
+    }
+}
