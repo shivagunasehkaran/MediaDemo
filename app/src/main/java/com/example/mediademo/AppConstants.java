6ce021a108ed1fc114103c177a7d006c88b6209f
shiva.gunasehkaran <shivagunasehkaran@rea-group.com>
2019-09-14 00:39:23 +0800

    App constants for service URL

---
+++ app/src/main/java/com/example/mediademo/AppConstants.java
@@ -0,0 +1,7 @@
+package com.example.mediademo;
+
+public interface AppConstants {
+    String API_BASE_URL = "https://pixabay.com/";
+    String API_KEY = "10961674-bf47eb00b05f514cdd08f6e11";
+    String API_PAGE = "1";
+}
