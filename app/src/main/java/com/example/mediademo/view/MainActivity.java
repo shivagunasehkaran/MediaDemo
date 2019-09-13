47651d45769fabac2506e8cca6ccfa81d86c2476
shiva.gunasehkaran <shivagunasehkaran@rea-group.com>
2019-09-14 00:06:47 +0800

    Added butterknife

--- app/src/main/java/com/example/mediademo/view/MainActivity.java
+++ app/src/main/java/com/example/mediademo/view/MainActivity.java
@@ -1,13 +1,28 @@
-package com.example.mediademo;
+package com.example.mediademo.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.mediademo.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.audio)
+    ImageView audio;
+
+    @BindView(R.id.gallery)
+    ImageView gallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // bind the view using butterknife
+        ButterKnife.bind(this);
     }
 }
